5c91679dccb9e458bcce4bc504ba85cb74c4161a
Ivelin Shenkov <ishenkito@gmail.com>
2023-09-23 14:33:56 +0300

    Add express and handlebars configuration

---
+++ src/config/expressConfig.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+function expressConfig(app) {
+    // Express config (path подаваме релативен път и го получаваме в абсолютен)
+    app.use(express.static(path.resolve(__dirname, '../public')));
+    // body parser (парсва request`a , ако имаме изпратени POST данни от форма през request`a този парсър ще ги парсне в обикновен обект т.е после през req.body можем да видим всички изпратени данни)
+    app.use(express.urlencoded({ extended: false }))
+}
+
+module.exports = expressConfig;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+
+const expressConfig = require('./config/expressConfig')
+const handlebarsConfig = require('./config/handlebarsConfig');
 
 const app = express();
 
 const PORT = 5000;
 
-// Express config (path подаваме релативен път и го получаваме в абсолютен)
-// app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(express.static('src/public'));
+expressConfig(app);
+handlebarsConfig(app);
 
-// Handlebars config
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
 
 // Routes
 app.get('/', (req, res) => {
