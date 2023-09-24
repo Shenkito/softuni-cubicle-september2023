69221dc86100f609f7646ac6eb5e8e190cf3bfcf
Ivelin Shenkov <ishenkito@gmail.com>
2023-09-24 15:03:44 +0300

    Add home controller

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const expressConfig = require('./config/expressConfig')
 const handlebarsConfig = require('./config/handlebarsConfig');
 
+const homeController = require('./controllers/homeController');
+
 const app = express();
 
 const PORT = 5000;
@@ -10,10 +12,6 @@ const PORT = 5000;
 expressConfig(app);
 handlebarsConfig(app);
 
-
-// Routes
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(homeController);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
